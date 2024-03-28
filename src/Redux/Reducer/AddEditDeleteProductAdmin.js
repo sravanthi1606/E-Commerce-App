b152a8e599209429e28ee86cce45ea20f063622e
sravanthi1606 <151130251+sravanthi1606@users.noreply.github.com>
2024-03-28 08:54:00 +0530

    Updated AddEditDeleteProductAdmin.js

--- src/Redux/Reducer/AddEditDeleteProductAdmin.js
+++ src/Redux/Reducer/AddEditDeleteProductAdmin.js
@@ -6,19 +6,10 @@ export const AddEditDeleteReducer = (state = initialState, action) => {
     switch (action.type) {
 
         case "ADD_PRODUCT" : 
-        // return{
-        //     state:[...state,action.payload],
-        // }
-
         return {
             ...state,
             products: [...state.products, action.payload]
         };
-
-        // case  "EDIT_UPDATE_PRODUCT" :
-        //     const updateProduct = state.map((product)=>product.id === action.payload.id ? action.payload : product)
-        //     state = updateProduct;
-        //     return state;
 
 
         case "EDIT_UPDATE_PRODUCT":
